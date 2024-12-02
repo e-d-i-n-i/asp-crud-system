73f77e9696fa496cbd54bb960df427d3b87ed786
Edini Amare <edini.inbox@gmail.com>
2024-12-02 13:40:29 +0300

    course-create-read-success
    > implemented MVC CRUD operation for Courses
    > minor bugs on update and deletion
    > save point

---
+++ crud-system/Controllers/CoursesController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using crud_system.Models;
+using crud_system.Data;
+
+public class CoursesController : Controller
+{
+    private readonly CourseManagementContext _context;
+
+    public CoursesController(CourseManagementContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Courses
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Courses.ToListAsync());
+    }
+
+    // GET: Courses/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Courses/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("CourseID,CourseName,CourseChapter")] Course course)
+    {
+        if (ModelState.IsValid)
+        {
+            course.CourseID = Guid.NewGuid(); // Set the CourseID
+            _context.Add(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(course);
+    }
+
+    // GET: Courses/Edit/5
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var course = await _context.Courses.FindAsync(id);
+        if (course == null)
+        {
+            return NotFound();
+        }
+        return View(course);
+    }
+
+    // POST: Courses/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Guid id, [Bind("CourseID,CourseName,CourseChapter")] Course course)
+    {
+        if (id != course.CourseID)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(course);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Courses.Any(e => e.CourseID == course.CourseID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(course);
+    }
+
+    // GET: Courses/Delete/5
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var course = await _context.Courses
+            .FirstOrDefaultAsync(m => m.CourseID == id);
+        if (course == null)
+        {
+            return NotFound();
+        }
+
+        return View(course);
+    }
+
+    // POST: Courses/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(Guid id)
+    {
+        var course = await _context.Courses.FindAsync(id);
+        _context.Courses.Remove(course);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
